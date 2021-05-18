10e0ae55a50e4981d16b5e2ca4e4b08e1c240ed9
GorillaDev <germansanz93@gmail.com>
2021-05-18 14:07:20 -0300

    -intro ended -mobile navbar started

--- script.js
+++ script.js
@@ -1,9 +1,12 @@
 const box = document.querySelector('.box');
 const gLetter = document.querySelector('.g-letter');
 const sLetter = document.querySelector('.s-letter');
+const main = document.querySelector('.container');
 
-window.addEventListener('load', () => {
-    box.style.opacity = '1';
-    gLetter.style.opacity = '1';
-    sLetter.style.opacity = '1';
+gLetter.addEventListener('animationend', () => {
+    box.style.opacity = 0;
+    box.addEventListener('transitionend', () => {
+        main.style.opacity = 1;
+        document.body.style.backgroundColor = 'var(--background-blue)'
+    });
 })
