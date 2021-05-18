699aff6486d2fb17aa6cf6a2e8346658767a7642
GorillaDev <germansanz93@gmail.com>
2021-05-18 00:05:47 -0300

    -some styles added

---
+++ script.js
@@ -0,0 +1,9 @@
+const box = document.querySelector('.box');
+const gLetter = document.querySelector('.g-letter');
+const sLetter = document.querySelector('.s-letter');
+
+window.addEventListener('load', () => {
+    box.style.opacity = '1';
+    gLetter.style.opacity = '1';
+    sLetter.style.opacity = '1';
+})
