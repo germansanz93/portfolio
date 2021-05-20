c36a5aa360f8157e115d7678dbdd9c02b273f8ec
GorillaDev <germansanz93@gmail.com>
2021-05-19 23:44:34 -0300

    Added a lot of styles, worked in the menu for mobile

--- script.js
+++ script.js
@@ -1,12 +1,35 @@
+//intro
 const box = document.querySelector('.box');
 const gLetter = document.querySelector('.g-letter');
 const sLetter = document.querySelector('.s-letter');
-const main = document.querySelector('.container');
+const container = document.querySelector('.container');
+
 
 gLetter.addEventListener('animationend', () => {
     box.style.opacity = 0;
     box.addEventListener('transitionend', () => {
+        container.style.opacity = 1;
+        document.body.style.backgroundColor = 'var(--background-blue)';
         main.style.opacity = 1;
-        document.body.style.backgroundColor = 'var(--background-blue)'
+        document.body.removeChild(box);
     });
+})
+
+
+
+//content
+const menuBtn = document.querySelector('.menu-btn');
+const menuIcon = document.querySelector('.menu-icon');
+const menuList = document.querySelector('.menu-list');
+const main = document.querySelector('main')
+const menuItems = document.querySelectorAll('.menu-link');
+
+menuBtn.addEventListener('click', () => {
+    menuList.classList.toggle('show');
+    menuList.classList.contains('show') ? menuIcon.innerText = 'menu_open' : menuIcon.innerText = 'menu';
+})
+
+let index = 0;
+menuItems.forEach((link) => {
+    link.before(`0${++index}. `);
 })
