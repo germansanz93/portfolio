e9343dc11d7931d37b21d269c1d998f7e3e1f278
GorillaDev <germansanz93@gmail.com>
2021-05-27 22:33:09 -0300

    carousel btns functionallity skill section with icons

--- script.js
+++ script.js
@@ -1,6 +1,25 @@
+//menu
 const menuList = document.querySelector('.menu-list');
 const menuBtn = document.querySelector('.menu-btn');
 
 menuBtn.addEventListener('click', () => {
     menuList.classList.toggle('show');
+})
+
+//carousel
+const carousel = document.querySelector('.carousel');
+const prevBtn = document.querySelector('.carousel-prev');
+const nextBtn = document.querySelector('.carousel-next');
+
+
+prevBtn.addEventListener('click', () => {
+    carousel.scrollLeft > visualViewport.width/40 ? 
+        carousel.scrollBy({top: 0, left: -visualViewport.width, behavior: 'smooth'}) :
+        carousel.scrollLeft = visualViewport.width/40;
+})
+
+nextBtn.addEventListener('click', () => {
+    carousel.scrollLeft < 2 * visualViewport.width*(39/40) ? 
+        carousel.scrollBy({top: 0, left: visualViewport.width, behavior: 'smooth'}) :
+        carousel.scrollLeft = 2 * visualViewport.width*(39/40);
 })
