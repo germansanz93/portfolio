7287fca4f6f4804aa088129b4e567ea132f05d3d
germansanz93 <germansanz93@gmail.com>
2021-07-07 19:18:44 -0300

    added a lot of styles mf related

--- script.js
+++ script.js
@@ -2,6 +2,15 @@
 const menuList = document.querySelector('.menu-list');
 const menuBtn = document.querySelector('.menu-btn');
 const menuLinks = document.querySelectorAll('.menu-link');
+const nav = document.querySelector('nav');
+const firstCard = document.querySelector('.card');
+const projects = document.querySelector('#projects');
+
+
+//carousel
+const carousel = document.querySelector('.carousel');
+const prevBtn = document.querySelector('.carousel-prev');
+const nextBtn = document.querySelector('.carousel-next');
 
 menuLinks.forEach(link => {
     link.addEventListener('click', () => {
@@ -11,18 +20,33 @@ menuLinks.forEach(link => {
 
 menuBtn.addEventListener('click', () => {
     menuList.classList.toggle('show');
+    menuBtn.classList.toggle('show');
 })
-
-//carousel
-const carousel = document.querySelector('.carousel');
-const prevBtn = document.querySelector('.carousel-prev');
-const nextBtn = document.querySelector('.carousel-next');
-
 
 prevBtn.addEventListener('click', () => {
     carousel.scrollBy({top: 0, left: -carousel.offsetWidth, behavior: 'smooth'});
 })
 
 nextBtn.addEventListener('click', () => {
-        carousel.scrollBy({top: 0, left: carousel.offsetWidth, behavior: 'smooth'});
+    carousel.scrollBy({top: 0, left: carousel.offsetWidth, behavior: 'smooth'});
 })
+
+window.addEventListener('scroll',() => {
+    let yPosition = window.pageYOffset;
+    yPosition >= 85 ? nav.classList.add('hide') : nav.classList.remove('hide');
+    menuList.classList.remove('show');
+    menuBtn.classList.remove('show');
+})
+
+window.addEventListener('load', carouselBtnsDisplay)
+window.addEventListener('resize', carouselBtnsDisplay)
+
+function carouselBtnsDisplay(){
+    if(firstCard.offsetWidth*3 <= projects.offsetWidth){
+        nextBtn.style.visibility = 'hidden';
+        prevBtn.style.visibility = 'hidden';
+    } else {
+        nextBtn.style.visibility = 'visible';
+        prevBtn.style.visibility = 'visible';
+    }
+}
